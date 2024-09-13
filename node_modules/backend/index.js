6449b0647cda53b9ada26fb6e8a437c39b5b4acc
Ashura2003 <shreejanm21@gmail.com>
2024-09-13 23:02:42 +0545

    added packages

---
+++ node_modules/backend/index.js
@@ -0,0 +1,19 @@
+// Importing express package
+const express = require('express');
+
+// Importing cors package
+const cors = require('cors');
+
+// Importing body-parser package
+const bodyParser = require('body-parser');
+
+// Importing express-fileupload package
+const acceptFormData = require('express-fileupload');
+
+// Importing dotenv package
+const dotenv = require('dotenv');
+
+
+
+// Configuring dotenv
+dotenv.config();
