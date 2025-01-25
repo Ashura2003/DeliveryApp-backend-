4e7599dea8d73e899c8e8fd746f7b60373d584cd
Ashura2003 <shreejanm21@gmail.com>
2025-01-25 20:53:34 +0545

    Created delete user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -159,10 +159,49 @@ const editUserProfile = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
+
+    const {password}  = req.body;
+
+    if (!password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please fill all the fields" });
+    }
+    
+
+    const user = await userModel.findById(req.user.id);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+
+    await user.remove();
+
+    res.status(200).json({ success: true, message: "User deleted successfully" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   userLogin,
   userRegister,
-  userProfile,
+
   editUserProfile,
   userProfile,
+  deleteUser,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,7 @@ router.post("/register", userController.userRegister);
 router.post("/login", userController.userLogin);
 router.get("/get_user", authGuard, userController.userProfile);
 router.post("/edit_user", authGuard, userController.editUserProfile);
+router.post("/delete_user", authGuard, userController.deleteUser);
 
 //Export Router
 module.exports = router;
